49f7423c865b342c61d84e384abc1fbc210ded4f
fhd <fahadali2973747@gmail.com>
2024-01-09 01:29:36 +0500

    ui improved

--- app/src/main/java/com/example/bmicalculator20/Result.java
+++ app/src/main/java/com/example/bmicalculator20/Result.java
@@ -8,18 +8,19 @@ import android.view.View;
 
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity {
-    MaterialButton calculate;
+public class Result extends AppCompatActivity {
+    MaterialButton reclc ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        calculate = findViewById(R.id.calculate);
-        calculate.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_result);
+        reclc = findViewById(R.id.recalculate);
+        reclc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this , Result.class);
-                startActivity(i);
+                Intent intent = new Intent(Result.this , MainActivity.class);
+                startActivity(intent);
             }
         });
     }
